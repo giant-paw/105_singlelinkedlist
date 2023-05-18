1f92d770d78df890ad0b0e39e957236f31ecd041
giant-paw <giantpawbusiness@gmail.com>
2023-05-18 11:38:35 +0700

    menambahkan kode untuk menghapus data dari linked list

--- singlelinkedlist_105/singlelinkedlist_105.cpp
+++ singlelinkedlist_105/singlelinkedlist_105.cpp
@@ -147,3 +147,24 @@ int main() {
 				system("cls");
 				break;
 
+			case 2:
+				if (listEmpty()) {
+					cout << "List Kosong" << endl;
+					system("pause");
+					system("cls");
+					break;
+				}
+
+				int nim;
+				cout << "Masukkan NIM : ";
+				cin >> nim;
+
+				if (deleteNode(nim)) {
+					cout << "nim : " << nim << " berhasil dihapus" << endl;
+					system("pause");
+					system("cls");
+				}
+				else
+					cout << "Data tidak ditemukan" << endl;
+				break;
+
